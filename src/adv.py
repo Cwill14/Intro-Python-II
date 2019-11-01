927756cc35edcd6c4a2104ad822021eb6858f937
Clark Williams <theclarkwilliams@gmail.com>
2019-10-31 23:06:52 -0600

    still haven't fixed issue of referencing item object based on item name string

--- src/adv.py
+++ src/adv.py
@@ -1,6 +1,8 @@
 from room import Room
 from player import Player
+from item import Item
 import re
+import sys
 # Declare all the rooms
 
 room = {
@@ -8,18 +10,18 @@ room = {
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", [Item("Sword", "an ancient weapon, but still sharp")]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", [Item("staff", "elegant white staff")]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", [Item("empty chest", "recently opened")]),
 }
 
 # Link rooms together
@@ -32,37 +34,110 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
-#
+
 # Main
-#
+def get_play_input():
+    play_input = input("Ready? press 'enter' to continue")
+    if play_input == "enter":
+        print(my_player.current_room)
+        return
+    else:
+        pass
 def get_player():
     name_input = input("Set your character's name: ")
     global my_player
-    my_player = Player(name_input, room['outside'])
+    my_player = Player(name_input, room['outside'], [{Item("torch", "a simple torch")}, {Item("hat", "just a hat")}])
+    print("Commands:")
+    print("  n, e, s, w: move north, east, south, or west\n  r: see current room\n  p: see player information\n  i: see inventory\n  q: quit\n\n")
+    get_play_input()
     print(my_player.current_room)
 
 def get_input():
     global move_input
-    move_input = input("Enter directional command: ")
+    # move_input = sys.argv
+    move_input = input("Type command, press Enter to submit: ")
     game(move_input)
 
 def move(dir):
     x = getattr(my_player.current_room, str(dir), "can't go that way")
-    my_player.current_room = x
+    if x == "can't go that way":
+        print(x)
+        get_input();
+    else:
+        my_player.current_room = x
     print(my_player.current_room)
     get_input()
 
+def check_item(name, l):
+    # v = [True for i in l if i.name == item]
+    answer = [(i, value) for i, value in enumerate(l) if value.name == name]
+    print(f"answer: {answer}")
+    index = answer.i
+    print(f"index: {index}")
+    # v = dict(v)
+    # w = v
+    # print(f"w: {w}")
+    # for i in l:
+
+    return v
+
+def pickup(item_name):
+    room_result = check_item(item_name, my_player.current_room.room_items)
+    print(f"room_result: {room_result}")
+    print(f"room_result: {room_result[0]}")
+    print(f"room_result: {str(room_result)}")
+    print(f"room_result: {bool(room_result)}")
+    if room_result.name == item_name:
+        p_result = check_item(item_name, my_player.inventory)
+        print(f"p_result: {p_result}")
+        print(f"p_result type: {type(p_result)}")
+        print(f"type([]): {type([])}")
+
+        if p_result == []:
+            my_player.current_room.remove_item(room_result)
+            my_player.add_to_inventory(room_result)
+        else:
+            print(f"You already have the {item_name}")
+    else:
+        print(f"{item_name} does not exist in {my_player.current_room.name}")
+
+def drop(item):
+    my_player.drop_item(item)
+    my_player.current_room(item)
+    print(f"You have dropped the {item}")
 
 def game(m_input):
-
-    if re.search("[ensw]", m_input):
-       move(f"{m_input}_to")
-
-    elif m_input == 'q':
-        exit
+    x = re.split("\s", m_input)
+    if len(x) == 1:
+        if re.search("[ensw]", m_input):
+           move(f"{m_input}_to")
+        elif m_input == 'r':
+            print(my_player.current_room)
+            get_input()
+        elif m_input == "p":
+            print(my_player)
+            get_input()
+        elif m_input == "i":
+            print(my_player.inventory)
+            get_input()
+        elif m_input == "q":
+            exit
+        else:
+            print("Invalid key")
+            get_input()
     else:
-        print("Invalid key")
-        get_input()
+        verb = x[0]
+        noun = x[1]
+        # print(f"noun: {noun}")
+        if verb == "take":
+            pickup(noun)
+            get_input()
+        elif verb == "drop":
+            drop(noun)
+            get_input()
+        else:
+            print("Invalid key")
+            get_input()
 
 get_player()
 get_input()
--- src/player.py
+++ src/player.py
@@ -1,6 +1,22 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory=[]):
         self.name = name
         self.current_room = current_room
+        self.inventory = inventory
+
+    def __str__(self):
+        return f"Player Information: \n  Name: {self.name}\n  Location: {self.current_room.name}\n  Inventory: {self.inventory}"
+    
+    def __repr__(self):
+        return  f"Item({repr(self.name)})"
+
+    def add_to_inventory(self, item):
+        self.inventory.append(item)
+        print(f"You have picked up the {item}")
+
+    def drop_item(self, item):
+        self.inventory.remove(item)
+        print(f"You have dropped the {item}")
+
